ec36dffacecef3a593d09032a86982398c0dae07
Rashid <rraza87@gmail.com>
2022-08-29 12:58:42 +0530

    Update Run after startup

--- src/main/java/com/accolite/sim/RunAfterStartup.java
+++ src/main/java/com/accolite/sim/RunAfterStartup.java
@@ -1,7 +1,5 @@
 package com.accolite.sim;
 
-import com.accolite.sim.repository.CommissionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -9,13 +7,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class RunAfterStartup {
 
-    @Autowired
-    private CommissionRepository commissionRepository;
 
     @EventListener(ApplicationReadyEvent.class)
     public void runAfterStartup() {
-        System.out.println("=====================");
-        System.out.println("Server is up and running.");
-        System.out.println("=====================");
+        System.out.println("==========");
+        System.out.println("Server is up and running");
+        System.out.println("===========");
     }
 }
